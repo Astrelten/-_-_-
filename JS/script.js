05115995743add7023a17c3ce14ac1df3606659e
Astrelten <nastya.kudryavceva04@gmail.com>
2025-11-24 05:36:58 +0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function(){
+   document.getElementById("burger").addEventListener("click",function(){
+    document.querySelector(".header").classList.toggle("open")
+   })
+
+})
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    function initSwipers() {
+        if (window.innerWidth <= 576) {
+            const config = {
+                slidesPerView: 'auto',
+                spaceBetween: 16,
+                freeMode: true,
+                resistance: true,
+                resistanceRatio: 0
+            };
+            
+
+            if (document.querySelector('.popular__swiper').swiper) {
+                document.querySelector('.popular__swiper').swiper.destroy(true, true);
+            }
+            if (document.querySelector('.blog__swiper').swiper) {
+                document.querySelector('.blog__swiper').swiper.destroy(true, true);
+            }
+            if (document.querySelector('.gallery__swiper').swiper) {
+                document.querySelector('.gallery__swiper').swiper.destroy(true, true);
+            }
+            
+            new Swiper('.popular__swiper', config);
+            new Swiper('.blog__swiper', config);
+            new Swiper('.gallery__swiper', config);
+        }
+    }
+    
+    initSwipers();
+    window.addEventListener('resize', initSwipers);
+});
